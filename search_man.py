14a8386a30ac1b9be251867086a4282e3f7298ad
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2021-05-13 05:48:43 +0900

    to python 3.N

--- search_man.py
+++ search_man.py
@@ -1,18 +1,23 @@
 import datetime
-from mastodon import Mastodon, StreamListener
 import re
 import signal
 import sys
-import wikipedia
 
-wikipedia.set_lang('ja')
+import wikipedia
+from mastodon import Mastodon, StreamListener
+
+wikipedia.set_lang("ja")
 
 
 class SearchMan(StreamListener):
     @staticmethod
     def should_respect_ltl(old_status, new_status, min_seconds=10):
-        old_time = datetime.datetime.strptime(old_status['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        new_time = datetime.datetime.strptime(new_status['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        old_time = datetime.datetime.strptime(
+            old_status["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        new_time = datetime.datetime.strptime(
+            new_status["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
         return (new_time - old_time).seconds <= min_seconds
 
     @staticmethod
@@ -26,14 +31,19 @@ class SearchMan(StreamListener):
 
     @staticmethod
     def make_text_with_respect(acct):
-        status = '@{acct} '.format(acct=acct) + "やー、ちょっと疲れたんでパスで・・・"
+        status = "@{acct} ".format(acct=acct) + "やー、ちょっと疲れたんでパスで・・・"
         spoiler_text = None
         return status, spoiler_text
 
     @staticmethod
     def make_text_with_page(page, acct):
         spoiler_text = "なになに？「{title}」とは、".format(title=page.title)
-        status = '@{acct} '.format(acct=acct) + page.content[:450 - len(page.url)] + '.....\n' + page.url
+        status = (
+            "@{acct} ".format(acct=acct)
+            + page.content[: 450 - len(page.url)]
+            + ".....\n"
+            + page.url
+        )
         return status, spoiler_text
 
     @staticmethod
@@ -49,7 +59,7 @@ class SearchMan(StreamListener):
                 status=status,
                 spoiler_text=spoiler_text,
                 in_reply_to_id=in_reply_to_id,  # LTLに現れないので除外
-                visibility='private',
+                visibility="public",
             )
         except:
             # 文字数が多かったりするとエラーを吐くらしい
@@ -64,17 +74,19 @@ class SearchMan(StreamListener):
         self.post(self.mastodon, status, spoiler_text, status_id)
 
     def on_update(self, status):
-        content = self.re_remove_html_tag.sub('', status['content'])
+        content = self.re_remove_html_tag.sub("", status["content"])
         m = self.re_search.search(content)
         if m is None:
             return
 
         word = m.group(1)
         acct = status["account"]["acct"]
-        status_id = str(status['id'])
+        status_id = str(status["id"])
 
         # status history
-        if len(self.status_history) >= 1 and self.should_respect_ltl(self.status_history[0], status):
+        if len(self.status_history) >= 1 and self.should_respect_ltl(
+            self.status_history[0], status
+        ):
             status, spoiler_text = self.make_text_with_respect(acct)
             self.post(self.mastodon, status, spoiler_text, status_id)
             return
@@ -85,21 +97,21 @@ class SearchMan(StreamListener):
 
     def __init__(self):
         self.mastodon = Mastodon(
-            client_id='hiho_bot_app.secret',
-            access_token='hihobot1.secret',
-            api_base_url='https://friends.nico'
+            client_id="hiho_bot_app.secret",
+            access_token="hihobot1.secret",
+            api_base_url="https://friends.cafe",
         )
 
         self.status_history = []
-        self.re_remove_html_tag = re.compile(r'<[^>]+>')
-        self.re_search = re.compile('\s?(.+)って(何|なに|ナニ|何ですか|なんですか)？$')
+        self.re_remove_html_tag = re.compile(r"<[^>]+>")
+        self.re_search = re.compile("\s?(.+)って(何|なに|ナニ|何ですか|なんですか)？$")
 
     def start(self):
-        self.post(self.mastodon, '生き返りました')
-        self.mastodon.local_stream(self, async=False)
+        self.post(self.mastodon, "生き返りました")
+        self.mastodon.stream_local(self, run_async=False)
 
     def finish(self):
-        self.post(self.mastodon, '死にました')
+        self.post(self.mastodon, "死にました")
 
 
 def shutdown(*args):
@@ -108,7 +120,13 @@ def shutdown(*args):
 
 
 if True:
-    for sig in (signal.SIGABRT, signal.SIGILL, signal.SIGINT, signal.SIGSEGV, signal.SIGTERM):
+    for sig in (
+        signal.SIGABRT,
+        signal.SIGILL,
+        signal.SIGINT,
+        signal.SIGSEGV,
+        signal.SIGTERM,
+    ):
         signal.signal(sig, shutdown)
 
     instance = SearchMan()
